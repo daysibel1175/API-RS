2ac9f931f362f6d116d1aacb42bb7487b054527a
daysibel1175 <estergutierres1175@gmail.com>
2024-06-06 16:49:41 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,23 @@ import { routes } from "./routes";
 import cors from "@fastify/cors";
 
 const app = Fastify({ logger: true });
+const port = process.env.PORT || 2500;
 
 app.setErrorHandler((error, req, reply) => {
   reply.code(400).send({ message: error.message });
 });
-
+app.listen({
+  port:2500
+})
 const start = async () => {
-  await app.register(cors);
-  await app.register(routes);
   try {
-    await app.listen({ port: 2500 });
-    
+    await app.register(cors);
+    await app.register(routes);
+    console.log(`API REST funcionando en el puerto ${port}`);
   } catch (err) {
+    console.error("Error al iniciar el servidor:", err);
     process.exit(1);
   }
 };
+
 start();
